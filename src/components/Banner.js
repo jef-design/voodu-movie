e5cde47330f2625a80f0e390a8e7db6aa1b49d0b
jef-design <bermejojff97@gmail.com>
2021-06-28 11:58:09 +0800

    update vooduu

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ function Banner() {
         <section className="hero__section">
                 <div className="hero__container">
                         <div className="hero__text">
-                            <h1>Welcome to Voo<span>duu.</span> </h1>
+                            <h1>Welcome to Voo<span>du.</span> </h1>
                             <p>Millions of movies, TV shows and people to discover. Explore now.</p>
                         </div>
                 </div>
